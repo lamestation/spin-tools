7e94aa0b7fa1baeebd13811e0e6408dcbac77ff9
Brett Weir <brett@lamestation.com>
2016-06-13 19:54:41 -0700

    Initial commit

---
+++ makefile
@@ -0,0 +1,33 @@
+ifndef SPINC
+	SPINC := openspin -q
+endif
+
+LFLAGS   := -L library/
+FOLDERS  := library/
+
+OBJECTS  := $(shell find $(FOLDERS) -name \*.spin )
+BINARIES := $(OBJECTS:.spin=.binary)
+ZIPS     := $(shell find . -name \*.zip)
+
+PREFIX ?= .build
+
+INSTALLS := $(patsubst %,$(PREFIX)/%,$(OBJECTS))
+
+
+all: test
+
+clean:
+	rm -f $(BINARIES) $(ZIPS)
+	rm -rf $(PREFIX)
+
+release:
+	./scripts/release.py $(RELEASE_TAG)
+
+test: test_compile
+
+test_compile: $(BINARIES)
+
+%.binary: %.spin
+	@FILE=$$(file $< | grep -v "ASCII" | grep -v "UTF-8"); if [ -z "$$FILE" ]; then exit 0 ; else exit 1; fi
+	@FILE=$$(file $< | grep "line terminators"); if [ -z "$$FILE" ]; then exit 0 ; else exit 1 ; fi
+	$(SPINC) $(LFLAGS) $<
---
+++ scripts/release.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+
+import os, sys, shutil
+import errno    
+import subprocess
+import zipfile
+import fnmatch
+import re
+
+def zipdir(path, ziph):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file))
+
+def mkdir(path):
+    print 'dir', path
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+def purge(directory, pattern):
+    for root, dirnames, filenames in os.walk(directory):
+        for filename in fnmatch.filter(filenames, pattern):
+            print "del", os.path.join(root, filename)
+            os.remove(os.path.join(root, filename))
+
+def command(cmd):
+    try:
+        out = subprocess.check_output(cmd)
+    except subprocess.CalledProcessError as e:
+        print "Error: Command failed"
+        sys.exit(1)
+    return out
+
+
+
+
+version = 'master'
+
+if len(sys.argv) > 1:
+    version = sys.argv[1]
+
+release = "lamestation-sdk-"+version
+archive = release+".zip"
+builddir = ".build"
+docsdir = os.path.join(builddir, 'docs')
+
+print 'version', version
+print 'release', release
+
+try:
+    shutil.rmtree(builddir)
+except:
+    pass
+shutil.copytree("library",builddir)
+
+purge(builddir, "*.sh")
+purge(builddir, "*.xcf")
+purge(builddir, "*.svg")
+
+mkdir(docsdir)
+print 'wget'
+command(['wget','-4','-q','-O',
+    os.path.join(docsdir,'lamestation-book-'+version+'.pdf'),
+    'https://www.gitbook.com/download/pdf/book/bweir/lamestation-book'])
+
+files = command(["git","ls-tree","-r",version,"--name-only"]).splitlines()
+files = [f for f in files if re.match(r'.*\.spin', f)]
+
+for line in files:
+    filebase = os.path.basename(line)
+
+    if filebase.startswith('gfx'):  continue
+    if filebase.startswith('map'):  continue
+    if filebase.startswith('snd'):  continue
+    if filebase.startswith('ins'):  continue
+    if filebase.startswith('song'): continue
+    if filebase.startswith('wav'):  continue
+
+    filecomps = line.split(os.sep)
+    filecomps.pop(0)
+    filename = os.sep.join(filecomps)
+    newfilename = os.path.join(builddir, filename)
+
+    print 'meta', line
+    logs = command(["git","log", "--format=%aD","--follow", line]).splitlines()
+    today = logs[0].split()[3]
+    created = logs[-1].split()[3]
+
+    age = created + "-" + today
+    if created == today:
+        age = today
+
+    f = open(line, 'r').read()
+
+    output = ""
+    output += "' "+80*'-'+"\n"
+    output += "' "+filename+"\n"
+    output += "' Version: "+version+"\n"
+    output += "' Copyright (c) "+age+" LameStation LLC\n"
+    output += "' See end of file for terms of use.\n"
+    output += "' "+80*'-'+"\n"
+
+    output += f
+
+    output += "\n\nDAT\n"
+    output += open(os.path.join('scripts','LICENSE.MIT'),'r').read()
+    output += "DAT\n"
+
+    f = open(newfilename, 'w')
+    f.write(output)
+    f.close()
+
+try:
+    shutil.rmtree(release)
+except OSError:
+    pass
+shutil.move(builddir, release)
+
+try:
+    os.remove(archive)
+except:
+    pass
+
+print 'zip', archive
+zipf = zipfile.ZipFile(archive, 'w', zipfile.ZIP_DEFLATED)
+zipdir(release, zipf)
+zipf.close()
+
+shutil.move(release, builddir)
+
+print 'done'
